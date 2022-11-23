06b1c5d44b729a118b4dcedbfedb46784ce9881a
Camilo Campo <mcp.ccampo@gmail.com>
2022-11-22 23:55:15 -0500

    completed assessment

---
+++ Assessment/assessment-js-basics-1/gitInfo.js
@@ -0,0 +1,85 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet 
+    for common git commands. You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+let gitDefinition = "its a version control application that allows you to manage a code locally on your computer"
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDefinition = "its version control web appication that allows you to manage a code stored in the cloud used by individuals as well a development teams"
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+let gitInitDefinition = "this git command initializes a git repository"
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDefinition = "This git command downloads a local copy of a repository stored in a internet git server"
+
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+let gitStatusDefinition = 'this git command shows you the status of your local git repository'
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+let gitAddDefinition = 'this git command adds tracking to a file or a directory of files'
+
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+let gitCommitDefinition = 'this git command works in hand with git add, to commit the changes to a file after has tracking enabled'
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = 'this git command pushes all your code changes to the github internet server'
---
+++ Assessment/assessment-js-basics-1/sdlc.js
@@ -0,0 +1,28 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set 
+    the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis = "collect business objective, gather process diagrams and issue a final analyis"
+const Design = "document and ilustrate the operation of the system and its features"
+const Implementation = "code accordintly to the design specifications, and deploye the solution"
+const Testing = "check solution for issues and works as expected"
+const Maintain = "apply fixes or enhancements to the solution in order to improve and provide up to date functionality"
